bcc2dd3e9abe70b047e235127a5e52b3a04d0e7b
LasseJantsch <lassejantsch@web.de>
2021-07-07 20:15:43 +0200

    show client in server

--- server/app.js
+++ server/app.js
@@ -13,24 +13,31 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
 //set static assets
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(path.join(__dirname, '../client/build')
+))
 
 
 
 //set routes
-const index = require('./routes/index.js');
-app.use('/', index);
+// const index = require('./routes/index.js');
+// app.use('/', index);
 
 
 //connect to MongoDB
-const dbURI = process.env.MONGODB
-mongoose.connect(dbURI, { useNewUrlParser:  true, useUnifiedTopology: true })
-    .then((result) => {
-        console.log("connected to MondoDB")
-        //set Port 
-        const PORT = process.env.PORT 
+// const dbURI = process.env.MONGODB
+// mongoose.connect(dbURI, { useNewUrlParser:  true, useUnifiedTopology: true })
+//     .then((result) => {
+//         console.log("connected to MondoDB")
+//         //set Port 
+//         const PORT = process.env.PORT 
+//         app.listen(PORT, () => {
+//             console.log(`listening on Port: ${PORT}`)
+//         })
+//     })
+//     .catch((err) => console.log(err))
+
+
+    const PORT = process.env.PORT 
         app.listen(PORT, () => {
             console.log(`listening on Port: ${PORT}`)
         })
-    })
-    .catch((err) => console.log(err))
