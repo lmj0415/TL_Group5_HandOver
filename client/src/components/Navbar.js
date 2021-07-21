4862c8f81ac6945487561b8a8c01a8b2e837b78a
Nadine <nchabrie@yahoo.de>
2021-07-21 18:28:05 +0200

    Adjusting Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import Logo from '../logo.png'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-       <nav className="nav-wrapper red darken-3">
+       <nav className="nav-wrapper">
         <div className="container">
-            <a className="brand-logo">Hand Over</a>
-            <ul className="right">
-                <li><a href="/">Home</a></li>
-                <li><a href="/needy">Needy</a></li>
-                <li><a href="/donator">Donator</a></li>
-                <li><a href="/gastro">Gastro</a></li>
-                <li><a href="/contact">Contact</a></li>
+            <img src={Logo} alt="Hand Over Logo"/>
+            <ul className="right hide-on-med-and-down">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/needy">Needy</Link></li>
+                <li><Link to="/donator">Donator</Link></li>
+                <li><Link to="/gastro">Gastro</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
             </ul>
         </div> 
        </nav>
