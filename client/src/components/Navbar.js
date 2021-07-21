df7342b63ec69ea0f648a1912b9352f77fa34f8a
Nadine <nchabrie@yahoo.de>
2021-07-21 23:04:00 +0200

    Burger Menu integrated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import Logo from '../logo.png'
-import { Link, NavLink } from 'react-router-dom'
+import { bubble as Menu } from 'react-burger-menu'
 
-const Navbar = () => {
+class Navbar extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+
+  render () {
     return (
-       <nav className="nav-wrapper">
+      <nav className="nav-wrapper">
         <div className="container">
             <img src={Logo} alt="Hand Over Logo"/>
-            <ul className="right hide-on-med-and-down">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/needy">Needy</Link></li>
-                <li><Link to="/donator">Donator</Link></li>
-                <li><Link to="/gastro">Gastro</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </div> 
-       </nav>
-    )
-} 
+            <h1>Hand Over</h1>
+            <h2>Yes, we care!</h2>
+            <Menu right>
+              <a id="home" className="menu-item" href="/">Home</a>
+              <a id="about" className="menu-item" href="/needy">Needy</a>
+              <a id="about" className="menu-item" href="/donator">Donator</a>
+              <a id="about" className="menu-item" href="/gastro">Gastro</a>
+              <a id="contact" className="menu-item" href="/contact">Contact</a>
+              <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+            </Menu>
+        </div>
+      </nav>
 
-
+    );
+  }
+}
 export default Navbar
