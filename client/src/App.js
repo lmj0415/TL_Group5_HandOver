abb871f2ede715ffc9a6c30d318458a602cd3f59
LasseJantsch <lassejantsch@web.de>
2021-08-24 08:00:35 +0200

    Contact css

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,39 @@
 import React, {Component} from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route} from 'react-router-dom'
 import Home from './components/Home'
 import Needy from './components/Needy'
 import Gastro from './components/Gastro'
 import Donator from './components/Donator'
 import Contact from './components/Contact'
 // import Stories from "./components/CMS/Stories"
-import CMS from "./components/CMS/CMSComponent"
+import CMS from './components/CMS'
 import { ContactProvider } from './Context/ContactContext'
+import { CMSProvider } from './Context/CMSContext'
 
 
-class App extends Component{
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-              <Route exact path='/' component={Home} />
-              <Route path='/needy' component={Needy} />
-              <Route path='/gastro' component={Gastro} />
-              <Route path='/donator' component={Donator} />
-              <ContactProvider> 
-                <Route path='/contact' component={Contact} />
-              </ContactProvider>
-             
-              {/* <Route path="/stories" component = {Stories} />  */}
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        {window.location.pathname !== "/cms" ? <Navbar />: null}
+            <Route exact path='/' component={Home} />
+            <Route path='/needy' component={Needy} />
+            <Route path='/gastro' component={Gastro} />
+            <Route path='/donator' component={Donator} />
+            <ContactProvider> 
+              <Route path='/contact' component={Contact} />
+            </ContactProvider>
+            <CMSProvider>
               <Route path="/cms" component = {CMS} /> 
-          <Footer/>
-        </div>
-      </BrowserRouter>
-    );
-  }
+            </CMSProvider>
+            
+         {window.location.pathname !== "/cms" ? <Footer/>: null}
+      </div>
+    </BrowserRouter>
+  ); 
 }
 
 
