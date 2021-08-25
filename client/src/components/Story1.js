dd34424d078bfc8db92e812d853a2be024ea8392
Nadine <nchabrie@yahoo.de>
2021-08-25 22:35:57 +0200

    set up landingpages Story2 & Story3 (not working yet), NC

--- client/src/components/Story1.js
+++ client/src/components/Story1.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './Slider/Slider.css'
 import dataStorySlider from './Slider/dataStorySlider'
 
@@ -9,16 +9,16 @@ return(
     <div className="Intro-Module">
             <img src={process.env.PUBLIC_URL + `/Imgs/${story.img}`} alt="Man looking into the streets"/>
             {/* <div className="Text-Element-Background"></div> */}
-            <div className="Text-Element">
-                <p>A social project to enrich our society.</p>
+            <div className="Text-Element-Intro">
+                
                 <h1 className="center">{story.title}</h1> 
+                <p>{story.subTitle}</p>
             </div>
         
         </div>
         <div className="Brand-Text">
             <h3>Together we are better. <br/>Let's enrich social life.</h3>
-            <p>Fate is not always kind. Some of us have to face difficulties in their lives which are sometimes hart to overcome. Therefore not all of us are able to participate in social life due to low income levels or other challenges. 
-            <br/>A small gesture of kindness can help in multiple ways. With a small donation a meal can be shared.</p>
+            <p>{story.story}</p>
         </div>
     </div>
 
