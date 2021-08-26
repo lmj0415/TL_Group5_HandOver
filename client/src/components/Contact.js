339f53faa9961c90e0b1c3f871c3b3b0692af940
Nadine <nchabrie@yahoo.de>
2021-08-26 12:29:29 +0200

    Style Contact Form + new Story Images, NC

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -17,20 +17,25 @@ const Contact = () => {
     }
 
     return (
-        <>
+        <div className="Contact">
             <IntroModule 
             subheading= "If you want more Information"
             heading= {"Contact us now!"}/>
             <form>
+                <label className="ipt">Last & first name</label>
                 <input className="ipt" name="author" placeholder="Name" value={message.author} onChange={handleChange}></input>
-                <input className="ipt" type="email" name="email"  placeholder="E-Mail" value={message.email} onChange={handleChange}></input>
-                <input className="ipt" name="betreff"  placeholder="Betreff" value={message.betreff} onChange={handleChange}></input>
+                <label className="ipt">Mail address</label>
+                <input className="ipt" name="email" placeholder="E-Mail" value={message.email} onChange={handleChange}></input>
+                <label className="ipt">Subject</label>
+                <input className="ipt" name="betreff"  placeholder="Subject" value={message.betreff} onChange={handleChange}></input>
+                <label className="ipt">Your message</label>
                 <textarea className="ipt" name="message" placeholder="Message" value={message.message} onChange={handleChange}></textarea>
-                <input type="submit" name="submit" className="btn" onClick={submitMessage} value="Send" ></input>
-            </form>
-            {confirmation}
+                <input type="submit" name="submit" className="btn" onClick={submitMessage} value="Send ❯" ></input>
+             {confirmation}
+             </form>
+           
             
-        </>
+        </div>
     )
 }
 
